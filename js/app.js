8500942a81ce0e3aeb90c75f45f9efeda1ce4012
Balakhnin.Semyon <balakhnin.semyon@gmail.com>
2016-02-08 17:52:27 +0300

    Add reading from json

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 (function(angular) {
   'use strict';
   angular.module('ItLabsControl', [])
-  
+    .controller('TeachersController', ['$scope', '$http', function($scope, $http) {
+      $http.get('api/teachers.json').then(function(response) {
+        $scope.teachers = response.data;
+      });
+    }]);
 
 })(window.angular);
