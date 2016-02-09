783a992aa0c82994ac88490b93307b7620d32787
Balakhnin.Semyon <balakhnin.semyon@gmail.com>
2016-02-09 10:09:22 +0300

    Added directive <teachers-list> and service Teachers. Created external view for teachers list (views/teachers-list.html)

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 (function(angular) {
   'use strict';
   angular.module('ItLabsControl', [])
-    .controller('TeachersController', ['$scope', '$http', function($scope, $http) {
-      $http.get('api/teachers.json').then(function(response) {
+    .service('Teachers', ['$http', function($http) {
+      return {
+        getTeachers: function() {
+          return $http.get('api/teachers.json');
+        }
+      };
+    }])
+    .controller('TeachersController', ['$scope', 'Teachers', function($scope, Teachers) {
+      Teachers.getTeachers().then(function(response) {
         $scope.teachers = response.data;
       });
-    }]);
+    }])
+    .directive('teachersList', function() {
+      return {
+        restrict: 'E',
+        templateUrl: 'views/teachers-list.html',
+        controller: 'TeachersController'
+      };
+    })
 
 })(window.angular);
